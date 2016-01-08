b9bba1d5e316a6b62584e5e7abb8654491b2ab64
Quentin Madec <quentin.madec@datadoghq.com>
2016-01-08 09:54:58 +0100

    Use cli.go for cmd line interface

--- main.go
+++ main.go
@@ -1,23 +1,31 @@
 package main
 
 import (
+	"github.com/codegangsta/cli"
 	"github.com/degemer/document-search-engine/index"
 	"log"
+	"os"
 	"time"
 )
 
 func main() {
-	log.Println("Starting")
-	start_creation := time.Now()
-	i := index.New("tf-idf", map[string]string{"cacm_path": "cacm/cacm.all", "common_words_path": "cacm/common_words"})
-	i.Create()
-	i.Save()
-	elapsed_creation := time.Since(start_creation)
-	log.Printf("Index creation took %s", elapsed_creation)
+	app := cli.NewApp()
+	app.Name = "document-search-engine"
+	app.Usage = "Search database"
+	app.Action = func(c *cli.Context) {
+		log.Println("Starting")
+		start_creation := time.Now()
+		i := index.New("tf-idf", map[string]string{"cacm_path": "cacm/cacm.all", "common_words_path": "cacm/common_words"})
+		i.Create()
+		i.Save()
+		elapsed_creation := time.Since(start_creation)
+		log.Printf("Index creation took %s", elapsed_creation)
 
-	start_load := time.Now()
-	j := index.New("tf-idf", map[string]string{"cacm_path": "cacm/cacm.all", "common_words_path": "cacm/common_words"})
-	j.Load()
-	elapsed_load := time.Since(start_load)
-	log.Printf("Index load took %s", elapsed_load)
+		start_load := time.Now()
+		j := index.New("tf-idf", map[string]string{"cacm_path": "cacm/cacm.all", "common_words_path": "cacm/common_words"})
+		j.Load()
+		elapsed_load := time.Since(start_load)
+		log.Printf("Index load took %s", elapsed_load)
+	}
+	app.Run(os.Args)
 }
