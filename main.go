0c0b995f96102e6703726bcae7bc592f21fffefa
Quentin Madec <quentin.madec@datadoghq.com>
2016-01-30 17:40:17 +0100

    Clean main.go (divide in functions)

--- main.go
+++ main.go
@@ -13,117 +13,133 @@ import (
 
 func main() {
 	app := cli.NewApp()
-	index_name := "tf-idf"
-	cacm_path := "cacm"
-	alpha := "0.5"
-	beta := "1.0"
 	app.Name = "document-search-engine"
 	app.Usage = "Search database"
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name:        "index, i",
-			Value:       "tf-idf",
-			Usage:       "Index to be used",
-			Destination: &index_name,
+			Name:  "index, i",
+			Value: "tf-idf",
+			Usage: "Index to be used",
 		},
 		cli.StringFlag{
-			Name:        "cacm",
-			Value:       "cacm",
-			Usage:       "Path to cacm directory",
-			Destination: &cacm_path,
-		},
-		cli.StringFlag{
-			Name:        "alpha",
-			Value:       "0.5",
-			Usage:       "Alpha value for E-Measure",
-			Destination: &alpha,
-		},
-		cli.StringFlag{
-			Name:        "beta",
-			Value:       "0.5",
-			Usage:       "Beta value for F-Measure",
-			Destination: &beta,
+			Name:  "cacm",
+			Value: "cacm",
+			Usage: "Path to cacm directory",
 		},
 	}
-	app.Commands = []cli.Command{
-		{
-			Name:    "index",
-			Aliases: []string{"i"},
-			Usage:   "Create index",
-			Action: func(c *cli.Context) {
-				options := make(map[string]string)
-				options["cacm"] = cacm_path
-				fmt.Println("Creating index", index_name)
-				i := index.New(index_name, options)
-				timeIndex(i.Create, "creation")
-				timeIndex(i.Save, "save")
-			},
-		},
-		{
-			Name:    "measure",
-			Aliases: []string{"m"},
-			Usage:   "Measure search pertinence",
-			Action: func(c *cli.Context) {
-				options := make(map[string]string)
-				options["cacm"] = cacm_path
-				options["alpha"] = alpha
-				options["beta"] = beta
-
-				fmt.Println("Loading index", index_name)
-				i := index.New(index_name, options)
-				loadOrCreateIndex(i)
-
-				search_name := "vectorial"
-				if len(c.Args()) > 0 {
-					search_name = c.Args()[0]
-				}
-				fmt.Println("Creating", search_name, "search")
-				searcher := search.New(search_name, i)
-
-				measurer := measure.New("cacm", options)
-				measurer.Measure(searcher)
-			},
-		},
-		{
-			Name:    "search",
-			Aliases: []string{"s"},
-			Usage:   "Search",
-			Action: func(c *cli.Context) {
-				options := make(map[string]string)
-				options["cacm"] = cacm_path
-
-				fmt.Println("Loading index", index_name)
-				i := index.New(index_name, options)
-				loadOrCreateIndex(i)
-
-				search_name := "vectorial"
-				if len(c.Args()) > 0 {
-					search_name = c.Args()[0]
-				}
-				fmt.Println("Creating", search_name, "search")
-				searcher := search.New(search_name, i)
-
-				reader := bufio.NewReader(os.Stdin)
-				fmt.Println("Enter request (empty request to exit): ")
-				req, _ := reader.ReadString('\n')
-				for req != "\n" {
-					start_search := time.Now()
-					results := searcher.Search(req)
-					fmt.Println("Search took", time.Since(start_search), "printing 10 out of", len(results))
-
-					for i, res := range results {
-						if i < 10 {
-							fmt.Println("Res", i, ":", res.Id, "-", res.Score)
-						}
-					}
-					fmt.Println()
-					fmt.Println("Enter request: ")
-					req, _ = reader.ReadString('\n')
-				}
-			},
-		},
-	}
+	app.Commands = append([]cli.Command{}, index_command(), search_command(), measure_command())
 	app.Run(os.Args)
+}
+
+func index_command() cli.Command {
+	return cli.Command{
+		Name:    "index",
+		Aliases: []string{"i"},
+		Usage:   "Create index",
+		Action: func(c *cli.Context) {
+			options := make(map[string]string)
+			options["cacm"] = c.GlobalString("cacm")
+			fmt.Println("Creating index", c.GlobalString("index"))
+			i := index.New(c.GlobalString("index"), options)
+			timeIndex(i.Create, "creation")
+			timeIndex(i.Save, "save")
+		},
+	}
+}
+
+func search_command() cli.Command {
+	c := cli.Command{
+		Name:    "search",
+		Aliases: []string{"s"},
+		Usage:   "Search",
+		Action: func(c *cli.Context) {
+			options := make(map[string]string)
+			options["cacm"] = c.GlobalString("cacm")
+			nbResults := c.Int("n")
+
+			fmt.Println("Loading index", c.GlobalString("index"))
+			i := index.New(c.GlobalString("index"), options)
+			loadOrCreateIndex(i)
+
+			search_name := "vectorial"
+			if len(c.Args()) > 0 {
+				search_name = c.Args()[0]
+			}
+			fmt.Println("Creating", search_name, "search")
+			searcher := search.New(search_name, i)
+
+			reader := bufio.NewReader(os.Stdin)
+			fmt.Println("Enter request: ")
+			req, _ := reader.ReadString('\n')
+			for req != "\n" {
+				start_search := time.Now()
+				results := searcher.Search(req)
+				nbResultsPrinted := nbResults
+				if len(results) < nbResultsPrinted {
+					nbResultsPrinted = len(results)
+				}
+				fmt.Println("Search took", time.Since(start_search), "- printing", nbResultsPrinted, "out of", len(results))
+
+				for i, res := range results {
+					if i < nbResults {
+						fmt.Println("Res", i, ":", res.Id, "-", res.Score)
+					}
+				}
+				fmt.Println()
+				fmt.Println("Enter request (empty request to exit): ")
+				req, _ = reader.ReadString('\n')
+			}
+		},
+	}
+	c.Flags = []cli.Flag{
+		cli.IntFlag{
+			Name:  "n",
+			Value: 10,
+			Usage: "Number of results returned by request",
+		},
+	}
+	return c
+}
+
+func measure_command() cli.Command {
+	c := cli.Command{
+		Name:    "measure",
+		Aliases: []string{"m"},
+		Usage:   "Measure search pertinence",
+		Action: func(c *cli.Context) {
+			options := make(map[string]string)
+			options["cacm"] = c.GlobalString("cacm")
+			options["alpha"] = c.String("alpha")
+			options["beta"] = c.String("beta")
+
+			fmt.Println("Loading index", c.GlobalString("index"))
+			i := index.New(c.GlobalString("index"), options)
+			loadOrCreateIndex(i)
+
+			search_name := "vectorial"
+			if len(c.Args()) > 0 {
+				search_name = c.Args()[0]
+			}
+			fmt.Println("Creating", search_name, "search")
+			searcher := search.New(search_name, i)
+
+			measurer := measure.New("cacm", options)
+			measurer.Measure(searcher)
+		},
+	}
+	c.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "alpha",
+			Value: "0.5",
+			Usage: "Alpha value for E-Measure",
+		},
+		cli.StringFlag{
+			Name:  "beta",
+			Value: "1",
+			Usage: "Beta value for F-Measure",
+		},
+	}
+	return c
 }
 
 func timeIndex(fn func(), name string) {
